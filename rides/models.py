8b6a5271b417956ce698773fb2b0014bb0f6a91e
Nicholas Vickery <nv9344@princeton.edu>
2026-02-13 17:34:27 -0500

    adding defaults

--- rides/models.py
+++ rides/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class Person(models.Model):
     first_name = models.CharField(max_length=100)
-    origination_city = models.CharField(max_length=100)
-    origination_state = models.CharField(max_length=2)
+    origination_city = models.CharField(max_length=100, default='')
+    origination_state = models.CharField(max_length=2, default='')
     destination_city = models.CharField(max_length=100)
     destination_state = models.CharField(max_length=2)
     date = models.DateField()
