305944ba2e00723e411c7893f4b3e5f7544c443e
Nicholas Vickery <nv9344@princeton.edu>
2026-02-13 17:32:05 -0500

    new person model

--- rides/models.py
+++ rides/models.py
@@ -4,11 +4,12 @@ from django.db import models
 
 
 class Person(models.Model):
-  first_name = models.CharField(max_length=64)
-  origination = models.CharField(max_length=64)
-  destination_city = models.CharField(max_length=64)
-  destination_state = models.CharField(max_length=2)
-  date = models.DateField()
-  time = models.TimeField()
-  taking_passengers = models.BooleanField(default=False)
-  seats_available = models.IntegerField(default=0)
+    first_name = models.CharField(max_length=100)
+    origination_city = models.CharField(max_length=100)
+    origination_state = models.CharField(max_length=2)
+    destination_city = models.CharField(max_length=100)
+    destination_state = models.CharField(max_length=2)
+    date = models.DateField()
+    time = models.TimeField()
+    taking_passengers = models.BooleanField()
+    seats_available = models.IntegerField()
