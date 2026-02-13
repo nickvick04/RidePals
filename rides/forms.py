984a701bc4876193972b7729fe1abda05bf1973e
Nicholas Vickery <nv9344@princeton.edu>
2026-02-13 17:42:38 -0500

    comment changes

--- rides/forms.py
+++ rides/forms.py
@@ -4,10 +4,10 @@ from django import forms
 class RideForm(forms.Form):
   search = forms.CharField(
     label='Search by State', 
-    max_length=2,  # Changed from 64 to 2 for state abbreviations
+    max_length=2,
     required=False,
     widget=forms.TextInput(attrs={
       'placeholder': 'e.g., CA, NY, TX',
-      'class': 'form-control'  # Optional: Bootstrap styling
+      'class': 'form-control'
     })
   )
